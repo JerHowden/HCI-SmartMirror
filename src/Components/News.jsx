adf68abcb4bdd5f46e816ccdddd282329eb6f817
Eliascm17 <Elias.cmoreno17@gmail.com>
2019-10-08 23:04:17 -0500

    have app talking to hackernews api

--- src/Components/News.jsx
+++ src/Components/News.jsx
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
+import React, {useState, useEffect, Component } from 'react';
+import { getStory } from '../services/hnApi'
 
-export default class News extends Component {
+export const News = ({ storyId }) => {
+    const [story, setStory] = useState({});
 
-    constructor(props) {
-        super(props)
+    useEffect(() => {
+        getStory(storyId).then(data => data && data.url && setStory(data));
+    }, []);
 
-        this.state = {
-
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                News
-			</div>
-        )
-    }
-
-}
+    return story && story.url ? (
+        <>
+            <a href={story.url}>
+                <p>{story.title}</p>
+            </a>
+            By: <p>{story.by}</p>
+            Posted: <p>{story.time}</p>
+        </>
+    ) : null;
+};
