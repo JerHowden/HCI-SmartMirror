c15ac70d326b8ee81628e3b6e6c4e16dc8bb8b82
Elias Moreno <elias.cmoreno17@gmail.com>
2019-10-09 14:54:04 -0500

    News componenet fully talks to HN Api :)

--- src/services/hnApi.js
+++ src/services/hnApi.js
@@ -1,19 +1,19 @@
 import axios from 'axios';
 
 export const baseUrl = 'https://hacker-news.firebaseio.com/v0/';
-export const newStoriesUrl = `${baseUrl}newstories.json`;
-export const storyUrl = `${baseUrl}item/`;
+export const topStoriesURL = `${baseUrl}topstories.json`;
+export const aCertainStoryUrl = `${baseUrl}item/`;
 
 export const getStory = async (storyId) => {
     const result = await axios
-    .get(`${storyUrl + storyId}`)
+    .get(`${aCertainStoryUrl + storyId}.json`)
     .then(({ data }) => data)
 
     return result
 }
 
 export const getStoryIds = async () => {
-    const result = await axios.get(newStoriesUrl).then(({data}) => data);
+    const result = await axios.get(topStoriesURL).then(({data}) => data);
     
     return result;
 };
