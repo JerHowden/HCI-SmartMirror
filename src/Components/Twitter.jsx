675135481ed7e57863dfc43103d40e9d4dcd0d16
Elias Moreno <elias.cmoreno17@gmail.com>
2019-10-26 14:55:28 -0500

    changed to a component

--- src/Components/Twitter.jsx
+++ src/Components/Twitter.jsx
@@ -1,15 +1,28 @@
 // TwitterContainer.js
-import React from "react";
+import React, { Component } from 'react';
 import { TwitterTimelineEmbed } from "react-twitter-embed";
 
-const Twitter = () => {
-    return (
-        <div className="centerContent">
-            <div className="selfCenter standardWidth">
-                <TwitterTimelineEmbed sourceType="url" url="https://twitter.com/mashable/lists/social-media" options={{ height: 1600 }} />
-            </div>
-        </div>
-    );
-};
+export default class Twitter extends Component{
 
-export default Twitter;
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render() {
+        return (
+            <div className="centerContent">
+                <div className="selfCenter standardWidth">
+                    <TwitterTimelineEmbed sourceType="url" url="https://twitter.com/mashable/lists/social-media" options={{ height: 1600 }} />
+                </div>
+            </div>
+        )
+        
+    }
+    
+}
+
+// export default Twitter;
